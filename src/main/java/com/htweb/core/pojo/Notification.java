bc4762a5823ddd7b6114f6408ec31d870e77a22b
ximofam <vienpham177@gmail.com>
2026-05-31 07:13:34 +0700

    create notifications model

---
+++ src/main/java/com/htweb/core/pojo/Notification.java
@@ -0,0 +1,37 @@
+package com.htweb.core.pojo;
+
+
+import com.htweb.core.helpers.models.SoftDeleteModel;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notification extends SoftDeleteModel {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "message")
+    private String message;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "extras")
+    private Map<String, Object> extras;
+
+    @Column(name = "read_at")
+    private Instant readAt;
+}
